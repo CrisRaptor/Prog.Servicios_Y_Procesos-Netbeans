3b95d67c2536b1391dad9e05f7865cbe82e98e8f
CrisR <cristiangarcha7@gmail.com>
2024-11-28 12:45:16 +0100

    EntregaChat

--- T3/ChatSockets/src/chatsockets/SocketTCPClient.java
+++ T3/ChatSockets/src/chatsockets/SocketTCPClient.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 
 public class SocketTCPClient {
